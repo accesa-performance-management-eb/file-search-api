ebf3cb017a16153ca7d9c890bbfa98e8f7c483d6
Eusebiu Biroas <eusebiu@biroas.com>
2018-05-13 22:43:56 +0300

    add FileAttributes and FileType to File model

--- src/main/java/com/biroas/poc/file/search/api/service/FileIndexService.java
+++ src/main/java/com/biroas/poc/file/search/api/service/FileIndexService.java
@@ -1,12 +1,15 @@
 package com.biroas.poc.file.search.api.service;
 
-import com.biroas.poc.file.search.api.model.File;
-import com.biroas.poc.file.search.api.model.IndexResult;
+import com.biroas.poc.file.search.api.model.file.File;
+import com.biroas.poc.file.search.api.model.result.IndexResult;
 import com.biroas.poc.file.search.api.repository.FileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.inject.Inject;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Date;
 
 @Service
 public class FileIndexService {
@@ -22,8 +25,8 @@ public class FileIndexService {
         return fileRepository.save(file);
     }
 
-    public IndexResult indexDirectory(String path, boolean recursive) {
-        IndexResult indexResult=new IndexResult();
+    public IndexResult indexDirectory(String path, boolean recursive) throws IOException {
+        IndexResult indexResult = new IndexResult();
 
         java.io.File folder = new java.io.File(path);
         java.io.File[] listOfFiles = folder.listFiles();
@@ -42,6 +45,16 @@ public class FileIndexService {
             if (recursive && file.isDirectory()) {
                 count += indexDirectory(file.getParentDirectory() + java.io.File.separator + file.getFileName(), true)
                         .getIndexedDocuments();
+            }
+
+            if (!file.isDirectory()) {
+
+                BasicFileAttributes fileAttributes = Files.readAttributes(diskFile.toPath(), BasicFileAttributes.class);
+                file.getFileAttributes().setCreationDate(new Date(fileAttributes.creationTime().toMillis()));
+                file.getFileAttributes().setLastModifiedDate(new Date(fileAttributes.lastModifiedTime().toMillis()));
+                file.getFileAttributes().setLastAccessDate(new Date(fileAttributes.lastAccessTime().toMillis()));
+                file.getFileAttributes().setSize(fileAttributes.size());
+
             }
 
             this.indexFile(file);
--- src/main/java/com/biroas/poc/file/search/api/service/FileSearchService.java
+++ src/main/java/com/biroas/poc/file/search/api/service/FileSearchService.java
@@ -1,9 +1,8 @@
 package com.biroas.poc.file.search.api.service;
 
-import com.biroas.poc.file.search.api.model.File;
-import com.biroas.poc.file.search.api.model.SearchResult;
+import com.biroas.poc.file.search.api.model.file.File;
+import com.biroas.poc.file.search.api.model.result.SearchResult;
 import com.biroas.poc.file.search.api.repository.FileRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
