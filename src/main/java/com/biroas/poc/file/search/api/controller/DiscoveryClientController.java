964b346409c50b2424d16a130f8d0c0f5d94c1ef
Eusebiu Biroas <eusebiu.biroas@stayfriends.com>
2018-07-25 18:35:08 +0300

    address code smell: replace lambda with method reference

--- src/main/java/com/biroas/poc/file/search/api/controller/DiscoveryClientController.java
+++ src/main/java/com/biroas/poc/file/search/api/controller/DiscoveryClientController.java
@@ -1,6 +1,7 @@
 package com.biroas.poc.file.search.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +19,7 @@ public class DiscoveryClientController {
     public Optional<URI> serviceUrl() {
         return discoveryClient.getInstances("poc-file-search-api")
                 .stream()
-                .map(si -> si.getUri())
+                .map(ServiceInstance::getUri)
                 .findFirst();
     }
 
